8760ff2e125a9b58d4ea4aa0a1b98ed12b82963a
gemtunde <gemtunde@gmail.com>
2023-07-17 14:13:50 +0100

    landing page

--- app/page.js
+++ app/page.js
@@ -2,112 +2,186 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+<div className='bg-slate-100'>
+  {/* start nav */}
+   <div class="bg-[url('/img/hero.jpg')] hero__container">
+      <nav>
+         <div className='flex justify-between p-10 mx-20'>
+           <h3 className='text-2xl text-gray-200'>
+            <a href="#">  team   </a>
+            </h3>
+           <div className='flex gap-2'>
+              <li className='font-sm text-gray-200'><a href="#">Products</a></li>
+              <li className='font-sm text-gray-200'><a href="#">Blogs</a></li>
+              <li className='font-sm text-gray-200'><a href="#">Contact</a></li>
+              <li className='font-sm text-gray-200'><a href="#">Login</a></li>
+             <button className='bg-gray-500 text-white text-sm p-2'>Get Acess</button>
+           </div>
+
+         </div>
+      </nav>
+        {/*start Hero section */}
+     <div >
+        <div className='flex flex-col gap-2 p-10 mx-20'>
+          <h2 className='text-6xl text-gray-100'> Instant Collaboration for <br/> remote teams</h2>
+          <p className='text-sm text-gray-100'> All in one place for your remote team to chat, 
+          collaborate and track project progress</p>
+          <div>
+            <input type='text' placeholder='your email' className='p-2 mr-2' />
+            <button className='bg-blue-500 text-white p-2'>Get early access</button>
+          </div>
+          </div>
+     </div>
+
+   {/* end Hero Section */}
+   </div>
+   {/* end nav */}
+
+    {/* start team hub component */}
+       <div className='flex flex-col md:flex-row p-10 mx-20 mt-5'>
+           <div className='flex-1'>
+            {/* left-side component */}
+              <h2 className='text-5xl text-gray-600 mt-40 mb-10'>Your hub for team work</h2>
+              <p className='text-sm text-gray-400'>This summer’s Women’s World Cup is set to be the biggest and 
+                best yet, with more teams involved than ever before.
+                 It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+           </div>
+           <div className='flex-1'>
+                 {/* right-side component */}
+                   <Image
+                 src='/img/meetings.png'
+                      width={320}
+                      height={320}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+           </div>
+       </div>
+    {/* end team hub component */}
+
+
+        {/* start simple task component */}
+       <div className='flex flex-col md:flex-row p-10 mx-20 mt-5 gap-2'>          
+           <div className='flex-1'>
+                 {/* right-side component */}
+               
+                  <Image
+                   src='/img/photo1.jpg'
+                      width={320}
+                      height={320}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+           </div>
+            <div className='flex-1'>
+            {/* left-side component */}
+              <h2 className='text-5xl text-gray-600 mt-40 mb-10'>Simple Task Management</h2>
+              <p className='text-sm text-gray-400'>This summer’s Women’s World Cup is set to be the biggest and 
+                best yet, with more teams involved than ever before.
+                 It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+           </div>
+       </div>
+    {/* end simple task component */}
+
+     {/* start schedule component */}
+       <div className='flex flex-col md:flex-row p-10 mx-20 mt-5'>
+           <div className='flex-1'>
+            {/* left-side component */}
+              <h2 className='text-5xl text-gray-600 mt-40 mb-10'>Scheduling that actually works</h2>
+              <p className='text-sm text-gray-400'>This summer’s Women’s World Cup is set to be the biggest and 
+                best yet, with more teams involved than ever before.
+                 It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+           </div>
+           <div className='flex-1'>
+                 {/* right-side component */}
+               
+                  <Image
+                     src='/img/photo2.jpg'
+                      width={320}
+                      height={320}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+           </div>
+       </div>
+    {/* end schedule component */}
+ 
+
+   {/* team starts here */}
+     <div className='p-10 mx-20'>
+      <h2 className='text-5xl text-gray-800'>What people say about team</h2>
+     </div>
+     <div className='flex flex-col md:flex-row p-10 mx-20'>
+        <div className='bg-white flex flex-col flex-1 p-5 m-2'>
+          <h5>*********</h5>
+          <p className='text-sm text-gray-600'>  It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+          <div className='flex'>
+            
+                  <Image
+                   src='/img/photo1.jpg'
+                      width={20}
+                      height={20}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+                    <h4 className='text-xl font-bold text-gray-700'>Tunde Elesho</h4>
+          </div>
         </div>
-      </div>
+        <div className='bg-white flex flex-col flex-1 p-5 m-2'>
+          <h5>*********</h5>
+          <p className='text-sm text-gray-600'>  It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+          <div className='flex'>
+            
+                  <Image
+                   src='/img/photo1.jpg'
+                      width={20}
+                      height={20}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+                    <h4 className='text-xl font-bold text-gray-700'>Tunde Elesho</h4>
+          </div>
+        </div>
+        <div className='bg-white flex flex-col flex-1 p-5 m-2'>
+          <h5>*********</h5>
+          <p className='text-sm text-gray-600'>  It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+          <div className='flex'>
+            
+                  <Image
+                   src='/img/photo1.jpg'
+                      width={20}
+                      height={20}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+                    <h4 className='text-xl font-bold text-gray-700'>Tunde Elesho</h4>
+          </div>
+        </div>
+        <div className='bg-white flex flex-col flex-1 p-5 m-2'>
+          <h5>*********</h5>
+          <p className='text-sm text-gray-600'>  It means there are to be eight debutantes in Australia 
+                 and New Zealand, with some nations appearing in a World Cup for the first time at any level, on both the men’s and women’s side.</p>
+          <div className='flex'>
+            
+                  <Image
+                   src='/img/photo1.jpg'
+                      width={20}
+                      height={20}
+                     alt="meetings"
+                      className="object-contain ml-20"
+                    />
+                    <h4 className='text-xl font-bold text-gray-700'>Tunde Elesho</h4>
+          </div>
+        </div>
+     </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   {/* team ends here */}
+</div>
   )
 }
